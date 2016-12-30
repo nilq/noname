bd683b16f53189b6a45bd676282e3c51232f374b
Lord Niels N. Horn <i.am@nilq.dk>
2016-12-30 16:31:29 +0100

    Add models and model loading

--- src/dk/nilq/noname/test/Game.java
+++ src/dk/nilq/noname/test/Game.java
@@ -3,18 +3,41 @@ package dk.nilq.noname.test;
 import org.lwjgl.opengl.Display;
 
 import dk.nilq.noname.DisplayManager;
+import dk.nilq.noname.Renderer;
+import dk.nilq.noname.model.ModelLoader;
+import dk.nilq.noname.model.RawModel;
 
 public class Game {
 	private static final String TITLE = "noname";
 	
 	public static void main(String[] args) {
 		DisplayManager.createDisplay();
-		
+
 		Display.setTitle(TITLE);
+
+		ModelLoader loader = new ModelLoader();
+		Renderer renderer  = new Renderer();
+		
+		float[] vertices = {
+			-0.5f,  0.5f,  0f,
+			-0.5f, -0.5f,  0f, 
+			0.5f,  -0.5f,  0f,
+				
+			0.5f, -0.5f,  0f,
+			0.5f,  0.5f,  0f,
+			-0.5f, 0.5f,  0f,
+		};
+		
+		RawModel model = loader.loadToVAO(vertices);
 		
 		while(!Display.isCloseRequested()) {
+			renderer.prepare();
+			renderer.render(model);
+			
 			DisplayManager.updateDisplay();
 		}
+		
+		loader.cleanLists();
 		
 		DisplayManager.closeDisplay();
 	}
